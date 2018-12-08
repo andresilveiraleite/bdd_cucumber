5882a8664ac0a0c30a09c02728ccb7ae6d27c95d
André Leite <asmla@hotmail.com>
2018-12-08 21:03:46 -0200

    Adicionando 3 exemplo - Cucumber Contador

---
+++ exemplo3-cucumber-contador/src/test/java/ClasseJava_ChamaCucumber.java
@@ -0,0 +1,43 @@
+import com.sun.org.apache.xml.internal.security.utils.SignerOutputStream;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+// Crtl + Shift + O = Importa todas as bibliotecas necessárias entre o Java e o Cucumber.
+
+public class ClasseJava_ChamaCucumber {
+		
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+		System.out.println("Qualquer coisa");
+	}
+
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	}
+	
+	//Abaixo trata-se do cenário para exemplo do "Contador"
+	
+	private int contador = 0;
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+	   contador = arg1;
+	}
+
+	@Quando("^eu incrementar (\\d+)$")
+	public void euIncrementar(int arg1) throws Throwable {
+	    contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+	    System.out.println(arg1);
+	    System.out.println(contador);
+	    System.out.println(arg1 == contador);
+	}
+}
---
+++ exemplo3-cucumber-contador/target/test-classes/cucumber_exemplo.feature
@@ -0,0 +1,15 @@
+# language: pt
+Funcionalidade: Aprender Cucumber
+	Como um aluno
+	Eu quero aprender a utilizar Cucumber
+	Para que eu possa automatizar critérios de aceitação
+	
+Cenário: Deve executar a especifição
+	Dado que criei o arquivo corretamente
+	Quando executá-lo
+	Então a especificação deve finalizar com sucesso
+	
+Cenário: Deve incrementar contador
+	Dado que o valor do contador é 15
+	Quando eu incrementar 5
+	Então o valor do contador será 18
