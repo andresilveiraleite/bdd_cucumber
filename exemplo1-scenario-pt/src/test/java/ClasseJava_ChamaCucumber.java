2897a78fd518116e8b7ca44d6fd826d099f9ad45
André Leite <asmla@hotmail.com>
2018-12-08 18:39:06 -0200

    Adicionando 1 exemplo básico de BDD com cucumber e configurando para português

---
+++ exemplo1-scenario-pt/src/test/java/ClasseJava_ChamaCucumber.java
@@ -0,0 +1,21 @@
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+// Crtl + Shift + O = Importa todas as bibliotecas necessárias entre o Java e o Cucumber.
+
+public class ClasseJava_ChamaCucumber {
+
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	}
+
+	@Quando("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	}
+
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	}
+
+}
